7f3bde436e414ec2df576d967d0cde82e6e5196f
Andanchenko Maria <andanchenko.maria@gmail.com>
2022-12-17 16:13:26 +0200

    Update login.py

--- login.py
+++ login.py
@@ -21,16 +21,15 @@ def test_login():
     password_input_element = chrome_driver.find_element(By.CSS_SELECTOR, password_input_locator)
     password_input_element.clear()
     password_input_element.send_keys(password)
-    password_input_element.send_keys(Keys.ENTER)
+
     # Page object
     login_button_locator = '#login-button'
     login_button_element = chrome_driver.find_element(By.CSS_SELECTOR, login_button_locator)
     login_button_element.click()
     time.sleep(5)
     shopping_card_locator = "#shopping_cart_container"
-    shopping_card_element = chrome_driver.find_element(By.XPATH, shopping_card_locator)
+    shopping_card_element = chrome_driver.find_element(By.CSS_SELECTOR, shopping_card_locator)
     is_shopping_card = shopping_card_element.is_displayed()
 
     assert is_shopping_card is True, 'User was not logged-in'
-    shopping_card_element.screenshot('element_screenshot.jpg')
     chrome_driver.back()
